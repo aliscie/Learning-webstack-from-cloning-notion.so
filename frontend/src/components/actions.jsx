b26cc756246023bd05052d07599e4373d1fe9e48
ali <weplutus.1@gmail.com>
2020-08-07 14:29:52 +0300

    adding the button in the recursion instead of actions

--- frontend/src/components/actions.jsx
+++ frontend/src/components/actions.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import useElements from '../apiHooks/useElements'
-import ActionsButton from './button'
 function Actions({ i }) {
       const { data, put, setid, deleting, SetnewPost } = useElements();
       function update(event, i) {
@@ -9,7 +8,7 @@ function Actions({ i }) {
       }
       return (
             <div>
-                  <ActionsButton i={i} />
+
                   <div
                         style={{ display: 'inline-block' }}
                         onKeyUp={(event) => update(event, i)}
--- frontend/src/components/recursion.jsx
+++ frontend/src/components/recursion.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Actions from './actions'
+import ActionsButton from './button'
+
 function ItSelf({ data }) {
       return (
             <div>
@@ -13,6 +15,7 @@ function Recur({ data }) {
                   {data.map(i => {
                         return (
                               <div>
+                                    <ActionsButton i={i} />
                                     {i.tag === 'img' ? <i.tag src={i.src} /> : <Actions i={i} />}
                                     <div style={{ marginLeft: "20px" }}>
                                           {i.sub && <ItSelf data={i.sub} />}</div>
