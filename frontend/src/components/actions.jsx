236aec92f74b12a9ac30180e102afbf6d3379df0
ali <weplutus.1@gmail.com>
2020-08-08 16:28:10 +0300

    sperate the recursion

--- frontend/src/components/actions.jsx
+++ frontend/src/components/actions.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import useElements from '../apiHooks/useElements'
+import AddImage from './image'
 function Actions({ i }) {
       const { data, put, deleting, SetnewPost } = useElements();
       function update(event, i) {
@@ -9,16 +10,17 @@ function Actions({ i }) {
 
       return (
             <div>
+                  {i.tag === 'img' ? <AddImage i={i} /> :
 
-                  <div
-                        // problem: JSON.parse(i.style) return undifinde.
-                        // should I use PostgreSQL and use JSONField?
-                        onClick={() => { console.log(i.style) }}
-                        style={{ display: 'inline-block' }}
-                        onKeyUp={(event) => update(event, i)}
-                        contentEditable='true'>
-                        {i.text}
-                  </div>
+                        <div
+                              // problem: JSON.parse(i.style) return undifinde.
+                              // should I use PostgreSQL and use JSONField?
+                              onClick={() => { console.log(i.style) }}
+                              style={{ display: 'inline-block' }}
+                              onKeyUp={(event) => update(event, i)}
+                              contentEditable='true'>
+                              {i.text}
+                        </div>}
             </div>
       )
 }
