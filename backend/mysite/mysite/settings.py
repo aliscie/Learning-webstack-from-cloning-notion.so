dd03e850ccb713663ac26716cab3a315034278b5
ali <weplutus.1@gmail.com>
2020-08-12 07:42:05 +0300

    backend auth

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -26,8 +26,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+
+
 ]
+SITE_ID = 1
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/mysite/mysite/urls.py
+++ backend/mysite/mysite/urls.py
@@ -3,6 +3,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+
     path('admin/', admin.site.urls),
     path('', include('myapp.api.urls'))
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_EMAIL_REQUIRED = False
