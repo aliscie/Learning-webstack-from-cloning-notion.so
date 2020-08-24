5cdad2a3e408d3641b47bceef3945502162040cb
ali <weplutus.1@gmail.com>
2020-08-24 20:10:54 +0300

    resize opration 0.2

--- frontend/src/components/Boxshape.jsx
+++ frontend/src/components/Boxshape.jsx
@@ -5,20 +5,29 @@ function Boxshape({ className }) {
       const [w, setW] = useState(100)
       const [h, setH] = useState(100)
       const [isDown, setisDown] = useState(false)
+      const [dowPostion, setpostion] = useState({})
+      function handledown(e) {
+            setisDown(true)
+            setpostion({ prex: e.clientX, prey: e.clientY })
+      }
 
       function Handle({ className }) {
 
             const mouseOverHandler = (e) => {
                   if (isDown) {
-                        className == 'corner D' && setH(pre => { return pre - 20 })
+                        setW(w - (dowPostion.prex - e.clientX))
+                        setH(h - (dowPostion.prey - e.clientY))
+                        setpostion({ prex: e.clientX, prey: e.clientY })
+
+                        // className == 'corner D' && setH(pre => { return pre - (prex - x) })
                   }
             }
 
             return (
                   <div
-                        onMouseDown={() => setisDown(true)}
+                        onMouseDown={handledown}
                         onMouseUp={() => setisDown(false)}
-                        onMouseOver={mouseOverHandler}
+                        onMouseMove={mouseOverHandler}
                         className={`corner ${className}`}
                   ></div>
             )
@@ -30,10 +39,10 @@ function Boxshape({ className }) {
                   style={{ width: `${w}px`, height: `${h}px` }}
                   className='item'>
                   <div className='box'>
-                        <Handle className='corner A' />
-                        <Handle className='corner B' />
-                        <Handle className='corner C' />
-                        <Handle className='corner D' />
+                        <Handle className='A' />
+                        <Handle className='B' />
+                        <Handle className='C' />
+                        <Handle className='D' />
                   </div>
             </div>
       )
